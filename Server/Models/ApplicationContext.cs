39b3d5e2fbb74ce442e5974bb0ad793096a4cb22
Antonio <dontony69@gmail.com>
2021-09-17 20:25:47 +0300

    Forgot to secure my connection string
    But huh... it's not that easy to read from appsettings.json
    
    Will do this later maybe

--- Server/Models/ApplicationContext.cs
+++ Server/Models/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -34,6 +35,7 @@ namespace Server.Models
 
                 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=localhost;Database=Contoso_Authentication_Logs;User Id=sa;Password=Password123;");
+                
             }
         }
 
