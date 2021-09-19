325beb7cc69670e8bac35fa0d3745aa4425cfca1
Antonio <dontony69@gmail.com>
2021-09-19 14:31:44 +0300

    BetaController ?

--- Server/Controllers/RegistrationController.cs
+++ Server/Controllers/RegistrationController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -10,7 +11,7 @@ using Server.ViewModel;
 namespace Server.Controllers
 {
     [ApiController]
-    [Route("/api/registration/bymonth/[controller]")]
+    [Route("/api/registration/bymonth")]
     public class RegistrationController : ControllerBase
     {
             public RegistrationController()
@@ -55,13 +56,41 @@ namespace Server.Controllers
                 return new JsonResult(null);;
             }
 
-            // GET by Id action
+            [HttpGet("{id}")]
+            public ActionResult Get(int id)
+            {
+                if (Math.Floor(Math.Log10(id) + 1) > 6) // check for six digits
+                    return StatusCode(400);
+                int year = takeNDigits(id, 4);
+                int month = int.Parse((id % 100).ToString().PadLeft(2, '0'));
+                
+                if (!Enumerable.Range(1,12).Contains(month)) // check for a valid month
+                    return StatusCode(400);
+                if (!Enumerable.Range(2020,2021).Contains(year)) // check for a valid year
+                    return StatusCode(400);
+                
+                string str = "Year: " + year + " Month: " + month;
+                
+                
+                return new JsonResult(str);
+            }
 
             // POST action
 
             // PUT action
 
             // DELETE action
+            
+            private static int takeNDigits(int number, int N)
+            {
+                number = Math.Abs(number);
+                if(number == 0)
+                    return number;
+                int numberOfDigits = (int)Math.Floor(Math.Log10(number) + 1);
+                if (numberOfDigits >= N)
+                    return (int)Math.Truncate((number / Math.Pow(10, numberOfDigits - N)));
+                return number;
+            }
         
     }
 }
