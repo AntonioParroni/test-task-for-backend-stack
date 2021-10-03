c2b23658d20f23965964f8d87181e18c5f1dc1f8
Antonio <dontony69@gmail.com>
2021-10-03 14:27:34 +0300

    DbUpOkay

---
+++ DbUp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DbUp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Server=localhost;Database=testDB;User Id=sa;Password=Password123;";
+            
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+            
+            var result = upgrader.PerformUpgrade();
+            
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                Console.ReadLine();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
