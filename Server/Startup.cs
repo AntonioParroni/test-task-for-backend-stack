3549d6b35464254816d1a89c907ad866fd52f092
Antonio <dontony69@gmail.com>
2021-10-05 11:39:57 +0300

    Update Startup.cs

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -26,6 +26,11 @@ namespace Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+<<<<<<< Updated upstream
+=======
+
+            
+>>>>>>> Stashed changes
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" }); });
         }
