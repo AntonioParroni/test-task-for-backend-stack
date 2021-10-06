73a4221ff760c4c86c141e3ba889cc889c1bc8a7
Antonio <dontony69@gmail.com>
2021-10-06 17:39:38 +0300

    An update to RegistrationLogic

--- BLL/DapperRepo/RegistrationsRepo.cs
+++ BLL/DapperRepo/RegistrationsRepo.cs
@@ -7,6 +7,7 @@ using DAL.Models;
 using Dapper;
 using DTO;
 using Microsoft.Data.SqlClient;
+
 #pragma warning disable 8629
 #pragma warning disable 8602
 
@@ -29,24 +30,18 @@ namespace BLL.DapperRepo
 
         public List<CleanByMonth> GetRegistrationByCurrentMonth()
         {
-            List<CleanByMonth> infoListToReturn = new();
+            List<CleanByMonth> returnInfo = new List<CleanByMonth>();
             using (IDbConnection db = new SqlConnection(connectionString))
             {
                 int currMonth = DateTime.Today.Month;
-                var crudeData = db.Query<RegistrationCountByMonth>(
-                        $"SELECT * FROM RegistrationCountByMonth WHERE Month = {currMonth}")
-                    .ToList();
-                foreach (var crudeInfo in crudeData)
-                {
-                    CleanByMonth item = new()
-                    {
-                        year = crudeInfo.Year, month = crudeInfo.Month, registeredUsers = crudeInfo.NumberOfUsers
-                    };
-                    infoListToReturn.Add(item);
-                }
+                var sql =
+                    $"SELECT Year, Month, NumberOfUsers AS registeredUsers FROM RegistrationCountByMonth WHERE Month = {currMonth}";
+
+                var item = db.Query<CleanByMonth>(sql).ToList();
+                returnInfo = item;
             }
 
-            return infoListToReturn;
+            return returnInfo;
         }
 
         public CleanWithBoth GetRegistrationByID(int id)
@@ -54,31 +49,22 @@ namespace BLL.DapperRepo
             int year = MySimpleMath.TakeNDigits(id, 4);
             int month = int.Parse((id % 100).ToString().PadLeft(2, '0'));
             CleanWithBoth returnInfo = new();
-            returnInfo.year = year;
-            returnInfo.month = (byte)month;
-            returnInfo.registeredUsers = 0;
             List<Provision> specificData = new List<Provision>();
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-                var dataSet = db.Query<RegistrationCountByDevicesAndMonth>(
-                        $"SELECT * FROM RegistrationCountByDevicesAndMonth WHERE Year = {year} AND Month = {month}")
-                    .ToList();
-                foreach (var crudeInfo in dataSet)
-                {
-                    Provision info = new Provision();
-                    info.type = db.Query<DeviceType>(
-                            $"SELECT DeviceName FROM DeviceTypes WHERE DeviceID = {crudeInfo.DeviceType.Value}")
-                        .FirstOrDefault()
-                        .DeviceName;
-                    info.value = crudeInfo.NumberOfUsers;
-                    specificData.Add(info);
-                    if (crudeInfo.NumberOfUsers != null)
+                var sql =
+                    $"SELECT RCDM.Year, RCDM.Month, SUM(NumberOfUsers) OVER() AS registeredUsers, RCDM.NumberOfUsers as value, DT.DeviceName AS type FROM RegistrationCountByDevicesAndMonth RCDM " +
+                    " INNER JOIN DeviceTypes DT on DT.DeviceID = RCDM.DeviceType " +
+                    $" WHERE Year = {year} AND Month = {month} " +
+                    " group by RCDM.Month, RCDM.Year, RCDM.NumberOfUsers, DT.DeviceName";
+                var item = db.Query<CleanWithBoth, Provision, CleanWithBoth>(sql, (p, c) =>
                     {
-                        returnInfo.registeredUsers += crudeInfo.NumberOfUsers.Value;
-                    }
-                }
-
-                returnInfo.registeredDevices = specificData;
+                        specificData.Add(c);
+                        p.registeredDevices = specificData; // kinda ugly, but no other way..
+                        return p;
+                    }, splitOn: "value")
+                    .FirstOrDefault();
+                returnInfo = item;
             }
 
             return returnInfo;
